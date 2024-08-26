e80ae4c9b474af1ce400996cee810db87bf70ec6
Tashdik <s2110477153@ru.ac.bd>
2024-08-26 11:27:01 +0600

    floatingnav prop added

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -6,7 +6,7 @@ import { FloatingNav } from '../ui/floating-navbar';
 const HomePage = () => {
     return (
         <div>
-            <FloatingNav />
+            <FloatingNav navItems={navItems} />
             <OrbitingCirclesDemo />
             <StickyScrollRevealDemo />
         </div>
@@ -14,3 +14,23 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
+const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    //   icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+    //   icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    //   icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      
+    },
+  ];
