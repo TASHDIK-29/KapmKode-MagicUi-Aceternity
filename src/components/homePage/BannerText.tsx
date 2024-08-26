d503a96adb10bc881172523e5c29152a44feb773
Tashdik <s2110477153@ru.ac.bd>
2024-08-26 11:35:13 +0600

    bannertext proptype updated

--- src/components/homePage/BannerText.tsx
+++ src/components/homePage/BannerText.tsx
@@ -9,7 +9,7 @@ const BannerText = () => {
             </div>
             <div className='space-x-2  flex items-center justify-center'>
                 <button className='flex items-center gap-3 border-2 py-1 px-4 font-semibold rounded-sm text-base text-black'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#121212" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg> Start Learning Today
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#121212" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg> Start Learning Today
                 </button>
                 <button className='bg-violet-600 py-1 px-2 rounded-sm text-white text-base'>Join Now</button>
             </div>
